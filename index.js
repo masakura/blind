24145a3fc4a90e04a755cb8d8bf97bc8fe42d97c
Tomo Masakura <tomo.masakura@gmail.com>
2015-12-01 21:40:08 +0900

    show item list

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const app = require('./lib/server')();
+const database = require('./lib/database');
+const server = require('./lib/server');
 
-app.listen(3000);
+database.setup();
+console.log('database setup finish');
+
+server(database).listen(3000);
+console.log('run app http://localhost:3000');
---
+++ lib/setup.sql
@@ -0,0 +1,15 @@
+create table users(id, name);
+create table items(id, code, name, price);
+
+insert into users values(1, 'taro');
+insert into users values(2, 'jiro');
+insert into users values(3, 'sabro');
+insert into users values(4, 'shiro');
+insert into users values(5, 'goro');
+
+insert into items values(1, '978-4621066058', 'EFFECTIVE JAVA', 3888);
+insert into items values(2, '978-4873115658', 'リーダブルコード', 2592);
+insert into items values(3, '978-4274050190', '新装版 リファクタリング―既存のコードを安全に改善する', 4536);
+insert into items values(4, '978-4798116839', 'レガシーコード改善ガイド', 4526);
+insert into items values(5, '978-4873116143', 'テスト駆動開発による組み込みプログラミング', 3888);
+insert into items values(6, '978-4798110479', '組込みソフトウェア開発のための構造化モデリング', 2376);
