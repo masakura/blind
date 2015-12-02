dc701de5f5ecfd7b0d7ddb76f1281c13f81126da
Tomo Masakura <tomo.masakura@gmail.com>
2015-12-02 20:51:54 +0900

    アプリをappディレクトリへ移動

--- app/lib/server.js
+++ app/lib/server.js
@@ -4,7 +4,7 @@ const _ = require('underscore');
 const app = require('express')();
 
 function getTemplate(name) {
-  const filePath = path.join('templates', name);
+  const filePath = path.join(__dirname, '../templates', name);
   const templateFile = fs.readFileSync(filePath, 'utf-8');
   return _.template(templateFile);
 }
